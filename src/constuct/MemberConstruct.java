5c1061eb96d9fb12e2f82527a306b1d401fbbbef
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-10 16:38:25 +0900

    생성자

---
+++ src/constuct/MemberConstruct.java
@@ -0,0 +1,20 @@
+package constuct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    MemberConstruct(String name, int age) {
+        this(name, age, 50);
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+    }
+
+    MemberConstruct(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
