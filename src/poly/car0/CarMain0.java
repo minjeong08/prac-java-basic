1eeb9882fb2beaee6dd2147b5ac126762dab8819
minnjung0887921 <minjeong0887921@gmail.com>
2024-01-02 09:18:07 +0900

    model3car 추가

--- src/poly/car0/CarMain0.java
+++ src/poly/car0/CarMain0.java
@@ -7,5 +7,10 @@ public class CarMain0 {
         K3Car k3Car = new K3Car();
         driver.setK3Car(k3Car);
         driver.drive();
+
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
     }
 }
